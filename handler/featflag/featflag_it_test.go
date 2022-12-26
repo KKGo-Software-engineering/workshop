b0d7777b7e2d0062b300212b97eafa7a110cfd2c
aorjoa <bhuridech@gmail.com>
2022-12-26 21:42:03 +0700

    :white_check_mark: chore: add integration test to get featflag

---
+++ handler/featflag/featflag_it_test.go
@@ -0,0 +1,32 @@
+package featflag
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/kkgoo-software-engineering/workshop/internal/config"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetFeatFlagIT(t *testing.T) {
+	e := echo.New()
+
+	hFeatFlag := New(&config.Config{
+		FeatureFlag: config.FeatureFlag{IsLimitMaxSpend: true}})
+
+	e.GET("/features", hFeatFlag.List)
+
+	req := httptest.NewRequest(http.MethodGet, "/features", nil)
+	rec := httptest.NewRecorder()
+	e.ServeHTTP(rec, req)
+
+	if rec.Code != http.StatusOK {
+		t.Errorf("unexpected status code")
+	}
+
+	expected := `{"IsLimitMaxSpend": true}`
+	assert.Equal(t, http.StatusOK, rec.Code)
+	assert.JSONEq(t, expected, rec.Body.String())
+}
