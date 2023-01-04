57033294859a51710fcd615136a05b122a1b67e2
Nong AnuchitO <anuchitp@thoughtworks.com>
2023-01-04 23:03:05 +0700

    refactor rename file

---
+++ internal/middleware/mlog/middleware.go
@@ -0,0 +1,31 @@
+package mlog
+
+import (
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
+)
+
+func Middleware(logger *zap.Logger) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return logMiddleware(next, logger)
+	}
+}
+
+func logMiddleware(next echo.HandlerFunc, logger *zap.Logger) func(c echo.Context) error {
+	return func(c echo.Context) error {
+		l := logParentID(c, logger)
+		c.Set(key, l)
+		return next(c)
+	}
+}
+
+func logParentID(c echo.Context, logger *zap.Logger) *zap.Logger {
+	xParent := c.Request().Header.Get("X-Parent-ID")
+	if xParent == "" {
+		xParent = uuid.NewString()
+	}
+	xSpan := uuid.NewString()
+	return logger.With(zap.String("parent-id", xParent),
+		zap.String("span-id", xSpan))
+}
--- internal/middleware/mlog/mlog.go
+++ internal/middleware/mlog/mlog.go
@@ -1,7 +1,6 @@
 package mlog
 
 import (
-	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"go.uber.org/zap"
 )
@@ -15,28 +14,4 @@ func L(c echo.Context) *zap.Logger {
 	default:
 		return zap.NewNop()
 	}
-}
-
-func Middleware(logger *zap.Logger) echo.MiddlewareFunc {
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return logMiddleware(next, logger)
-	}
-}
-
-func logMiddleware(next echo.HandlerFunc, logger *zap.Logger) func(c echo.Context) error {
-	return func(c echo.Context) error {
-		l := logParentID(c, logger)
-		c.Set(key, l)
-		return next(c)
-	}
-}
-
-func logParentID(c echo.Context, logger *zap.Logger) *zap.Logger {
-	xParent := c.Request().Header.Get("X-Parent-ID")
-	if xParent == "" {
-		xParent = uuid.NewString()
-	}
-	xSpan := uuid.NewString()
-	return logger.With(zap.String("parent-id", xParent),
-		zap.String("span-id", xSpan))
 }
